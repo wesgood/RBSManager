5093f426df8eefa8b14fbd98da74755ee679f00c
PackNSlash <jpack2015@my.fit.edu>
2018-11-13 13:10:37 -0500

    Create TwistStampedMessage.swift
    Twist wrapper with Header (geometry_msgs/TwistStamped)

---
+++ RBSManager/Classes/Messages/Geometry/TwistStampedMessage.swift
@@ -0,0 +1,29 @@
+//
+//  TwistStampedMessage.swift
+//  ObjectMapper
+//
+//  Created by Jason Pack on 2018-11-13.
+//
+
+import UIKit
+import ObjectMapper
+
+public class TwistStampedMessage: RBSMessage {
+    public var header: HeaderMessage?
+    public var twist: TwistMessage?
+    
+    public override init() {
+        super.init()
+        header = HeaderMessage()
+        twist = TwistMessage()
+    }
+    
+    public required init?(map: Map) {
+        super.init(map: map)
+    }
+    
+    override public func mapping(map: Map) {
+        header <- map["header"]
+        twist <- map["twist"]
+    }
+}
