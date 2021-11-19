c5d7ee2033be9aa906097bfd95de566340eb655d
Esben Bücking-Rasmussen <esben.bueckingrasmussen@agcocorp.com>
2021-11-19 09:05:46 +0100

    Add SPM file

---
+++ Package.swift
@@ -0,0 +1,24 @@
+/ swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "RBSManager",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(
+            name: "RBSManager",
+            targets: ["RBSManager"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.1"),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.2"),
+    ],
+    targets: [
+        .target(
+            name: "RBSManager",
+            path: "RBSManager/Classes"
+            dependencies: []),
+    ]
+)
