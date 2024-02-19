accdb8f1ed483235333153547c604aabb18b35bd
Nicholas Robinson <{ID}+{username}@users.noreply.github.com>
2024-02-19 11:07:20 +0000

    Remove swiftlink from Package.swift to enable github-runner tests to complete

--- Package.swift
+++ Package.swift
@@ -13,16 +13,12 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.1"),
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.2.0"),
-        .package(url: "https://github.com/adamayoung/swiftlint-plugin.git", from: "0.51.0")
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", from: "4.2.0")
     ],
     targets: [
         .target(
             name: "RBSManager",
             dependencies: ["Starscream", "ObjectMapper"],
-            path: ".", sources: ["RBSManager"],
-            plugins: [
-                .plugin(name: "SwiftLintPlugin", package: "swiftlint-plugin")
-            ])
+            path: ".", sources: ["RBSManager"])
     ]
 )
